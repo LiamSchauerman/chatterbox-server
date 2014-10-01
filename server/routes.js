3f1dbbecf5270f22f0588f303513d4c92a19d78d
liamschauerman <ljschauerman@gmail.com>
2014-09-30 17:04:27 -0700

    removing old route logic

--- server/routes.js
+++ server/routes.js
@@ -6,42 +6,31 @@ module.exports = {
     utils.sendResponse(response, null, 204);
   },
   'GET': function(request, response) {
-    if (request.url === "/classes/messages") {
-      var readFile = function(callback) {
-        var options = {
-          encoding: 'utf8'
-        };
-        fs.readFile('./files/messages.txt', options, function(err, data) {
-          callback(err, data);
-        });
+    var readFile = function(callback) {
+      var options = {
+        encoding: 'utf8'
       };
-      readFile(function(err, data) {
-        data = data.replace(/\n/g, ",");
-        data = "["+data.slice(0,-2)+"}]";
-        var parsedData = JSON.parse(data);
-        var objToReturn = {
-          results: parsedData
-        };
-        utils.sendResponse(response, objToReturn);
+      fs.readFile('./files/messages.txt', options, function(err, data) {
+        callback(err, data);
       });
-    } else if (request.url === '/classes/room1'){
+    };
+    readFile(function(err, data) {
+      data = data.replace(/\n/g, ",");
+      data = "["+data.slice(0,-2)+"}]";
+      var parsedData = JSON.parse(data);
+      var objToReturn = {
+        results: parsedData
+      };
       utils.sendResponse(response, objToReturn);
-    }
+    });
   },
   'POST': function(request, response) {
-    if (request.url === "/classes/messages") {
-      utils.collectData(request, function(data) {
-        fs.appendFile('./files/messages.txt', data+"\n", function(err) {
-          if (err) throw err;
-          console.log("The 'data to append' was appended to file!");
-        });
+    utils.collectData(request, function(data) {
+      fs.appendFile('./files/messages.txt', data+"\n", function(err) {
+        if (err) throw err;
+        console.log("The 'data to append' was appended to file!");
       });
-      utils.sendResponse(response, null, 201);
-    } else if (request.url === '/classes/room1') {
-      request.on('data', function(data) {
-        objToReturn.results.push(JSON.parse(data));
-      });
-      utils.sendResponse(response, null, 201);
-    };
+    });
+    utils.sendResponse(response, null, 201);
   }
 }
